62729b685c5cd72ef1e0fcc9ceea812f23cd1bde
zinohe <zinoguev@gmail.com>
2020-12-17 17:56:01 -0600

    Order class created

---
+++ src/main/java/com/gbm/challenge/domains/Order.java
@@ -0,0 +1,57 @@
+
+package com.gbm.challenge.domains;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Order {
+
+	@Id
+	@GeneratedValue
+	private Long idOrder;
+	private Timestamp timestamp;
+	private String Operation;
+	private String IssuerName;
+	private Long TotalShares;
+	private Double SharePrice;
+	
+	public Order() {
+	}
+	public Long getIdOrder() {
+		return idOrder;
+	}
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getOperation() {
+		return Operation;
+	}
+	public void setOperation(String operation) {
+		Operation = operation;
+	}
+	public String getIssuerName() {
+		return IssuerName;
+	}
+	public void setIssuerName(String issuerName) {
+		IssuerName = issuerName;
+	}
+	public Long getTotalShares() {
+		return TotalShares;
+	}
+	public void setTotalShares(Long totalShares) {
+		TotalShares = totalShares;
+	}
+	public Double getSharePrice() {
+		return SharePrice;
+	}
+	public void setSharePrice(Double sharePrice) {
+		SharePrice = sharePrice;
+	}
+}
