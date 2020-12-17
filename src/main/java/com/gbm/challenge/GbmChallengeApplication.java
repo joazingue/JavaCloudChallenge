7722300629d1fdc14eac1c200a41c4e2edb00786
zinohe <zinoguev@gmail.com>
2020-12-16 20:02:36 -0600

    first commit

---
+++ src/main/java/com/gbm/challenge/GbmChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.gbm.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbmChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbmChallengeApplication.class, args);
+	}
+
+}
