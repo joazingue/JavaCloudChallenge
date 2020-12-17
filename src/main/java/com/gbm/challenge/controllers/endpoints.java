a794395c458d5d398daa2e7399dfb51766657e78
zinohe <zinoguev@gmail.com>
2020-12-17 17:56:50 -0600

    Account endpoint created

--- src/main/java/com/gbm/challenge/controllers/endpoints.java
+++ src/main/java/com/gbm/challenge/controllers/endpoints.java
@@ -4,7 +4,11 @@ package com.gbm.challenge.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gbm.challenge.domains.InvestmentAccount;
 
 @RestController
 public class endpoints {
@@ -13,5 +17,11 @@ public class endpoints {
 	public ResponseEntity<String> sayHello(){
 		return new ResponseEntity<String>("Hello from GBM challenge!", HttpStatus.OK);
 	}
-
+	
+	@PostMapping(path = "/accounts")
+	public ResponseEntity<InvestmentAccount> createAccount(@RequestBody String cash){
+		InvestmentAccount ia = new InvestmentAccount();
+		ia.setCash(Double.parseDouble(cash));
+		return new ResponseEntity<InvestmentAccount>(ia, HttpStatus.CREATED);
+	}
 }
